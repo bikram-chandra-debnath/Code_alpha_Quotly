1860dbd26d1667b6b0cdda96ffaba50279486f83
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-08-24 18:12:09 +0600

    Upload padding file

---
+++ lib/core/constants/padding.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quotly/core/constants/sizes.dart';
+
+class AppPadding {
+  AppPadding._();
+
+  static const EdgeInsetsGeometry padding = EdgeInsetsGeometry.symmetric(
+    horizontal: AppSizes.defaultSpace,
+  );
+}
